2b105caaa56756474b7d3540c728fb8296c2f01e
Samuel <samuel@uniris.io>
2022-04-19 11:58:21 +0200

    Setup elixir project

---
+++ lib/archethic_playground.ex
@@ -0,0 +1,18 @@
+defmodule ArchethicPlayground do
+  @moduledoc """
+  Documentation for `ArchethicPlayground`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ArchethicPlayground.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/archethic_playground_test.exs
@@ -0,0 +1,8 @@
+defmodule ArchethicPlaygroundTest do
+  use ExUnit.Case
+  doctest ArchethicPlayground
+
+  test "greets the world" do
+    assert ArchethicPlayground.hello() == :world
+  end
+end

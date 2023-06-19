ff3eb5f6b03b4448143f6271650d509527465a67
Clément Uster <clement.uster@gmail.com>
2023-06-19 14:20:04 +0000

    add the archethic logo + remove unused menu icon (#50)
    * add the archethic logo + remove unused menu icon
    
    * fix link on icon

--- lib/archethic_playground_web/live/components/sidebar_component.ex
+++ lib/archethic_playground_web/live/components/sidebar_component.ex
@@ -14,41 +14,15 @@ defmodule ArchethicPlaygroundWeb.SidebarComponent do
       <li class="relative px-6">
         <a
                 class="inline-flex items-center w-full text-sm font-semibold text-gray-800 transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200 dark:text-gray-100"
-                href="index.html"
+                href="#"
               >
-    <img phx-track-static src={ArchethicPlaygroundWeb.Router.Helpers.static_path(@socket, "/images/archethic_logo_gradient.svg")} class="w-6 h-6" />
+    <img phx-track-static src={ArchethicPlaygroundWeb.Router.Helpers.static_path(@socket, "/assets/images/ArchethicLogoalone-Gradient.svg")} class="w-6 h-6" />
         </a>
       </li>
     </ul>
     <!-- end Archethic logo -->
     <!-- Side Menu -->
     <ul class="mt-6">
-      <!-- menu.files -->
-      <li class="relative px-6 py-3">
-        <a
-                class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
-                href="#"
-                data-tippy-content="File Explorer"
-              >
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 01-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 011.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 00-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 01-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5a3.375 3.375 0 00-3.375-3.375H9.75" />
-          </svg>
-        </a>
-      </li>
-      <!-- end menu.files -->
-      <!-- menu.search -->
-      <li class="relative px-6 py-3">
-        <a
-                class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
-                href="#"
-                data-tippy-content="Search Files"
-              >
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="w-6 h-6" viewBox="0 0 16 16">
-            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
-          </svg>
-        </a>
-      </li>
-      <!-- end menu.search -->
       <!-- menu.validate -->
       <li class="relative px-6 py-3" x-data="{id: 1}">
         <a

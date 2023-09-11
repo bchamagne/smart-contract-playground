891ad7d4f355ccc0965e2f65bbc4c719e9bf4af7
Bastien Chamagne <bastien@archethic.net>
2023-09-11 10:18:24 +0200

    Contract.call_function mock

--- lib/archethic_playground/mock_functions.ex
+++ lib/archethic_playground/mock_functions.ex
@@ -1,104 +1,72 @@
 defmodule ArchethicPlayground.MockFunctions do
   @moduledoc false
-  @behaviour Archethic.Contracts.Interpreter.Library.Common.Chain
-  @behaviour Archethic.Contracts.Interpreter.Library.Common.Token
-  @behaviour Archethic.Contracts.Interpreter.Library.Common.Http
 
   alias ArchethicPlayground.Mock
+  alias Archethic.Contracts.Interpreter.Library
+
+  @behaviour Library.Common.Chain
+  @behaviour Library.Common.Token
+  @behaviour Library.Common.Http
 
   def prepare_mocks(mocks) do
     remove_existing_mocks()
     Enum.each(mocks, &add_mock/1)
   end
 
-  # this is not mockable
-  @impl Archethic.Contracts.Interpreter.Library.Common.Chain
+  # this is not mockable but still part of the behaviour
+  @impl Library.Common.Chain
   def get_burn_address() do
-    <<0::16, 0::256>> |> Base.encode16()
+    Library.Common.ChainImpl.get_burn_address()
   end
 
-  @impl Archethic.Contracts.Interpreter.Library.Common.Chain
+  # not a behaviour
+  def call_function(address, function, args) do
+    get_mocked_value("Contract.call_function/3", [address, function, args])
+  end
+
+  @impl Library.Common.Chain
   def get_genesis_address(address) do
-    case Process.get({"Chain.get_genesis_address/1", [address]}) do
-      nil ->
-        log_missing_mock_to_playground_console("Chain.get_genesis_address/1", address)
-        raise "missing_mock"
-
-      value ->
-        value
-    end
+    get_mocked_value("Chain.get_genesis_address/1", [address])
   end
 
-  @impl Archethic.Contracts.Interpreter.Library.Common.Chain
+  @impl Library.Common.Chain
   def get_first_transaction_address(address) do
-    case Process.get({"Chain.get_first_transaction_address/1", [address]}) do
-      nil ->
-        log_missing_mock_to_playground_console("Chain.get_first_transaction_address/1", address)
-        raise "missing_mock"
-
-      value ->
-        value
-    end
+    get_mocked_value("Chain.get_first_transaction_address/1", [address])
   end
 
-  @impl Archethic.Contracts.Interpreter.Library.Common.Chain
+  @impl Library.Common.Chain
   def get_genesis_public_key(public_key) do
-    case Process.get({"Chain.get_genesis_public_key/1", [public_key]}) do
-      nil ->
-        log_missing_mock_to_playground_console("Chain.get_genesis_public_key/1", public_key)
-        raise "missing_mock"
-
-      value ->
-        value
-    end
+    get_mocked_value("Chain.get_genesis_public_key/1", [public_key])
   end
 
-  @impl Archethic.Contracts.Interpreter.Library.Common.Chain
+  @impl Library.Common.Chain
   def get_transaction(address) do
-    case Process.get({"Chain.get_transaction/1", [address]}) do
-      nil ->
-        log_missing_mock_to_playground_console("Chain.get_transaction/1", address)
-        raise "missing_mock"
-
-      value ->
-        value
-    end
+    get_mocked_value("Chain.get_transaction/1", [address])
   end
 
-  @impl Archethic.Contracts.Interpreter.Library.Common.Token
+  @impl Library.Common.Token
   def fetch_id_from_address(address) do
-    case Process.get({"Token.fetch_id_from_address/1", [address]}) do
-      nil ->
-        log_missing_mock_to_playground_console("Token.fetch_id_from_address/1", address)
-        raise "missing_mock"
-
-      value ->
-        value
-    end
+    get_mocked_value("Token.fetch_id_from_address/1", [address])
   end
 
-  @impl Archethic.Contracts.Interpreter.Library.Common.Http
+  @impl Library.Common.Http
   def fetch(url) do
-    case Process.get({"Http.fetch/1", [url]}) do
-      nil ->
+    get_mocked_value("Http.fetch/1", [url],
+      on_miss: fn ->
         # we autorize normal fetch
-        Archethic.Contracts.Interpreter.Library.Common.HttpImpl.fetch(url)
-
-      value ->
-        value
-    end
+        Library.Common.HttpImpl.fetch(url)
+      end
+    )
   end
 
-  @impl Archethic.Contracts.Interpreter.Library.Common.Http
+  @impl Library.Common.Http
   def fetch_many(urls) do
-    case Process.get({"Http.fetch_many/1", [urls]}) do
-      nil ->
+    get_mocked_value("Http.fetch_many/1", [urls],
+      on_miss: fn ->
         # we autorize normal fetch_many
-        Archethic.Contracts.Interpreter.Library.Common.HttpImpl.fetch_many(urls)
-
-      value ->
-        value
-    end
+        Library.Common.HttpImpl.fetch_many(urls)
+      end
+    )
   end
 
   defp add_mock(mock = %Mock{}) do
@@ -111,7 +79,21 @@ defmodule ArchethicPlayground.MockFunctions do
     |> Enum.each(&Process.delete/1)
   end
 
-  defp log_missing_mock_to_playground_console(function, input) do
-    send(self(), {:console, :error, "Missing mock #{function} for input value: #{input}"})
+  defp get_mocked_value(name, args, opts \\ []) do
+    case Process.get({name, args}) do
+      nil ->
+        case Keyword.get(opts, :on_miss) do
+          nil ->
+            raise(Library.Error,
+              message: "#{name} is missing a mock for args: #{inspect(args)}"
+            )
+
+          fun ->
+            fun.()
+        end
+
+      value ->
+        value
+    end
   end
 end
